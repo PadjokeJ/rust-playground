ccf31d68fae67b393f7ff0bab8d67345b4845b42
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-06-16 11:47:47 +0200

    eratosthene sieve

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use clap::Parser;
+use std::time::{Instant};
+
+#[derive(Parser)]
+struct Max {
+    max: usize,
+}
+
+fn eratosthene(max: usize) -> Vec<i8> {
+    let mut nums : Vec<i8> = vec![0; max];
+
+    let start = Instant::now();
+    for i in 2..((max as f64).sqrt() as usize){
+        if nums[i] != -1{
+            for j in 2..(max/i){
+                nums[j * i] = -1;
+            }
+        }
+    }
+
+    let t = start.elapsed();
+    println!("program executed in {} seconds", t.as_secs_f32());
+
+     
+    for i in 2..max {
+        if nums[i] != -1 {
+            print!("{}, ", i);
+        }
+    }
+
+    return nums;
+} 
+
+fn main() {
+    let args = Max::parse();
+
+    eratosthene(args.max);
+}
